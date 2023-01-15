ec5c57b6296763aad42b328e7bc6c582dfb8ac80
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-01-15 12:29:08 +0900

    distribute index page to resume and home

--- bin/main.ml
+++ bin/main.ml
@@ -25,17 +25,31 @@ let () =
 
 let track_binary_update = Build.watch Sys.argv.(0)
 let resume_filename = "resume.html"
+let resume_file_fullname = Filename.concat destination resume_filename
+
+let home_filename = "home.md"
 
 let task =
   let open Build in
   create_file
     index_file_fullname
     (track_binary_update
-    >>> Yocaml_yaml.read_file_with_metadata (module Metadata.Page) resume_filename
+     >>> Yocaml_yaml.read_file_with_metadata (module Metadata.Page) home_filename
+     >>> Yocaml_markdown.content_to_html ()
     (* TODO(#10)(#11): Wrap "pandoc" command and implement "Yocaml_markdown.pandoc ~style:`Gfm ()" *)
     (* >>> Yocaml_markdown.content_to_html () *)
     >>> Yocaml_mustache.apply_as_template (module Metadata.Page) index_filename
     >>^ Stdlib.snd)
+
+let resume =
+  let open Build in
+  create_file
+    resume_file_fullname
+    (track_binary_update
+     >>> Yocaml_yaml.read_file_with_metadata (module Metadata.Page) resume_filename
+     >>> Yocaml_markdown.content_to_html ()
+     >>> Yocaml_mustache.apply_as_template (module Metadata.Page) index_filename
+     >>^ Stdlib.snd)
 
 let article = Filename.concat destination "articles"
 let article_filename = "article.html"
@@ -46,6 +60,8 @@ let () =
   Logs.info (fun m -> m "outputting index.html...ok")
 
 let blog =
+  Sys.readdir article
+  |> Array.iter (fun file -> Sys.remove (Filename.concat article file));
   process_files [ "./articles" ] (with_extension "md") (fun file ->
     let destination =
       basename file |> into article |> Helpers.flip replace_extension "html"
@@ -60,4 +76,4 @@ let blog =
       >>> Yocaml_mustache.apply_as_template (module Metadata.Article) article_filename
       >>^ Stdlib.snd))
 
-let () = Yocaml_unix.execute (task >> blog)
+let () = Yocaml_unix.execute (task >> resume >> blog)
---
+++ home.md
@@ -0,0 +1,13 @@
+# はじめに
+このサイトはkat0の活動についてまとめたものです。
+
+<a id="resume"></a>
+## [・Resume](/resume)
+私の職務経歴についてまとめています。  
+[Wantedly](https://www.wantedly.com/id/hd2g_kat0)にもまとめています。
+
+
+<a id="blog"></a>
+## [・Blog](/articles)
+その記事の執筆時点で、趣味関連のものをブログにまとめています。  
+仕事関連(ウェブサイト作成,AWS,etc...)については、[Zenn](https://zenn.dev/hd2g)にまとめています。
